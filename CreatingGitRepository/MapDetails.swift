34c4c6da498af9af2d236a8ba98cb4824db2f04c
Pranit <pranit4202@gmail.com>
2018-03-22 16:50:03 +0530

    New Branch for MapDetails View

---
+++ CreatingGitRepository/MapDetails.swift
@@ -0,0 +1,13 @@
+//
+//  MapDetails.swift
+//  CreatingGitRepository
+//
+//  Created by Mac 1 on 3/22/18.
+//  Copyright © 2018 OpenInfoTech. All rights reserved.
+//
+
+import UIKit
+
+class MapDetails: NSObject {
+
+}
