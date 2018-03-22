b8c6d1f2aff3e758480f567947444cef6a339ad4
Pranit <pranit4202@gmail.com>
2018-03-22 16:11:07 +0530

    Initial Commit

---
+++ CreatingGitRepository/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CreatingGitRepository
+//
+//  Created by Mac 1 on 3/22/18.
+//  Copyright © 2018 OpenInfoTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
