4be2beb9569862c85a800d4dc03a86be733eddab
Kajal <Kajal.openinfotech@gmail.com>
2018-03-23 12:26:45 +0530

    Add Another file for change developers name

---
+++ CreatingGitRepository/firstFile.swift
@@ -0,0 +1,17 @@
+//
+//  firstFile.swift
+//  CreatingGitRepository
+//
+//  Created by Mac 1 on 3/23/18.
+//  Copyright © 2018 OpenInfoTech. All rights reserved.
+//
+
+import UIKit
+
+class firstFile: NSObject {
+
+    func DoSomething()
+    {
+        print("Add File for change the name")
+    }
+}
